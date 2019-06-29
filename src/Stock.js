f1fe4def8cc1251deda03651cf23936433bfcc0f
andywarr <andywarr@gmail.com>
2019-06-29 13:07:02 -0700

    Updated change to 2dp

--- src/Stock.js
+++ src/Stock.js
@@ -7,7 +7,7 @@ const Stock = (props) => {
 
   return (
   	<div className={change >= 0 ? "card pos" : "card neg"}>
-      <h2 className="change">{`${change >= 0 ? "+"+change : change} (${changePercent.toFixed(2)}%)`}</h2>
+      <h2 className="change">{`${change >= 0 ? "+"+change.toFixed(2) : change.toFixed(2)} (${changePercent.toFixed(2)}%)`}</h2>
       <h1 className="ticker">{symbol}</h1>
       <h2 className="price">${latestPrice.toFixed(2)}</h2>
     </div>
